b1e5c2b77c9e04927f844a975ed812417d8d4f8d
alasiri <austinalasiri44@gmail.com>
2025-04-26 10:04:25 +0300

    understood tests

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+export function pointsForWord(word){
+    let points = 0;
+    for(const char of word){
+        if(/[?/"!@#%$^&*()' .,{}0-9]/.test(char)){
+            points += 0;
+        }else {
+        points += /[aeiou]/.test(char.toLowerCase()) ? 1:2
+        }
+    }
+    return points;
+}
